94144fa940863e6789882eb2e5baf1a8d7ff224c
Sam Gold <samg915757@gmail.com>
2021-05-18 16:18:33 -0600

    p5 practice

--- src/component/sketch.js
+++ src/component/sketch.js
@@ -8,12 +8,14 @@ export default (props) => {
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
-		p5.createCanvas(500, 500).parent(canvasParentRef);
+		p5.createCanvas(800, 800).parent(canvasParentRef);
 	};
 
 	const draw = (p5) => {
-		p5.background(0);
-		p5.ellipse(x, y, 70, 70);
+		p5.background(34, 78, 132);
+		p5.line(0, 800, 400, 400);
+		p5.ellipse(x, y, p5.random(100), p5.random(25, 450));
+		p5.triangle(x + 50, 750, 550, 800, 550, 600);
 		// NOTE: Do not use setState in the draw function or in functions that are executed
 		// in the draw function...
 		// please use normal variables or class properties for these purposes
